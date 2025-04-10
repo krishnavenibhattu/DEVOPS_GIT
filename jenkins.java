dc2a4e89efda6e7366a36d9248846a5193c56cf5
krishnavenibhattu <166016551+krishnavenibhattu@users.noreply.github.com>
2025-04-10 06:22:48 +0000

    Update jenkins.java

--- jenkins.java
+++ jenkins.java
@@ -7,4 +7,10 @@ System.out.println("HELLO DEVOPS_JENKINS");
 }
 
 
-#https://hub.docker.com/repository/docker/bhattukrishnaveni/myubuntu/general
+/*https://hub.docker.com/repository/docker/bhattukrishnaveni/myubuntu/general
+docker tag ubuntu:latest bhattukrishnaveni/myubuntu:latest
+(base) user@kittu:~$ docker push bhattukrishnaveni/myubuntu:latest
+The push refers to repository [docker.io/bhattukrishnaveni/myubuntu]
+3abdd8a5e7a8: Pushed 
+latest: digest: sha256:0b9e751164f0f576086bb03062186c5643fe6dc56223b6bfef0be2a9d4828c67 size: 529
+  */
